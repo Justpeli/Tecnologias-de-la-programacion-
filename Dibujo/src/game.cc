5a5fe10d1e0ce5d0f4a04504d8179e939a1c3848
Justpeli <zap407@amerike.edu.mx>
2021-09-09 18:41:50 -0500

    Proyecto terminado

--- Dibujo/src/game.cc
+++ Dibujo/src/game.cc
@@ -153,7 +153,161 @@ rectangle* Rectangle131{new rectangle(25, 25, 1050, 475, sf::Color(6,253,255))};
 rectangle* Rectangle132{new rectangle(50, 25, 1075, 475, sf::Color::White)};
 rectangle* Rectangle133{new rectangle(75, 25, 1125, 475, sf::Color::Black)};
 //Linea 16
-rectangle* Rectangle134{new rectangle(275, 25, 875, 500, sf::Color::Black)};
+rectangle* Rectangle134{new rectangle(25, 25, 725, 500, sf::Color::Black)};
+rectangle* Rectangle135{new rectangle(25, 25, 750, 500, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle136{new rectangle(25, 25, 775, 500, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle137{new rectangle(50, 25, 800, 500, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle138{new rectangle(25, 25, 850, 500, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle139{new rectangle(275, 25, 875, 500, sf::Color::Black)};
+rectangle* Rectangle140{new rectangle(50, 25, 1150, 500, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle141{new rectangle(25, 25, 1200, 500, sf::Color::Black)};
+//Linea 17
+rectangle* Rectangle142{new rectangle(25, 25, 725, 525, sf::Color::Black)};
+rectangle* Rectangle143{new rectangle(25, 25, 750, 525, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle144{new rectangle(25, 25, 775, 525, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle145{new rectangle(50, 25, 800, 525, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle146{new rectangle(25, 25, 850, 525, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle147{new rectangle(25, 25, 875, 525, sf::Color::Black)};
+rectangle* Rectangle148{new rectangle(50, 25, 900, 525, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle149{new rectangle(200, 25, 950, 525, sf::Color::Black)};
+rectangle* Rectangle150{new rectangle(25, 25, 1150, 525, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle151{new rectangle(25, 25, 1175, 525, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle152{new rectangle(25, 25, 1200, 525, sf::Color::Black)};
+//Linea 18
+rectangle* Rectangle153{new rectangle(50, 25, 725, 550, sf::Color::Black)};
+rectangle* Rectangle154{new rectangle(25, 25, 775, 550, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle155{new rectangle(50, 25, 800, 550, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle156{new rectangle(25, 25, 850, 550, sf::Color::Black)};
+rectangle* Rectangle157{new rectangle(50, 25, 875, 550, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle158{new rectangle(25, 25, 925, 550, sf::Color::Black)};
+rectangle* Rectangle159{new rectangle(50, 25, 950, 550, sf::Color::White)};
+rectangle* Rectangle160{new rectangle(50, 25, 1000, 550, sf::Color(245,151,0))};//Amarillo
+rectangle* Rectangle161{new rectangle(50, 25, 1050, 550, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle162{new rectangle(100, 25, 1100, 550, sf::Color::Black)};
+//Linea 19
+rectangle* Rectangle163{new rectangle(25, 25, 675, 575, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle164{new rectangle(50, 25, 700, 575, sf::Color::Black)};
+rectangle* Rectangle165{new rectangle(25, 25, 750, 575, sf::Color(245,151,0))};//Amarillo
+rectangle* Rectangle166{new rectangle(100, 25, 775, 575, sf::Color::Black)};
+rectangle* Rectangle167{new rectangle(50, 25, 875, 575, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle168{new rectangle(25, 25, 925, 575, sf::Color::Black)};
+rectangle* Rectangle169{new rectangle(50, 25, 950, 575, sf::Color::White)};
+rectangle* Rectangle170{new rectangle(50, 25, 1000, 575, sf::Color(245,151,0))};//Amarillo
+rectangle* Rectangle171{new rectangle(50, 25, 1050, 575, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle172{new rectangle(50, 25, 1100, 575, sf::Color::Black)};
+rectangle* Rectangle173{new rectangle(25, 25, 1150, 575, sf::Color(245,151,0))};//Amarillo
+rectangle* Rectangle174{new rectangle(150, 25, 1200, 575, sf::Color::Black)};
+//Linea 20
+rectangle* Rectangle175{new rectangle(25, 25, 675, 600, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle176{new rectangle(200, 25, 700, 600, sf::Color::Black)};
+rectangle* Rectangle177{new rectangle(50, 25, 900, 600, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle178{new rectangle(25, 25, 950, 600, sf::Color::Black)};
+rectangle* Rectangle179{new rectangle(50, 25, 975, 600, sf::Color::White)};
+rectangle* Rectangle180{new rectangle(25, 25, 1025, 600, sf::Color(245,151,0))};//Amarillo
+rectangle* Rectangle181{new rectangle(25, 25, 1050, 600, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle182{new rectangle(100, 25, 1075, 600, sf::Color::Black)};
+rectangle* Rectangle183{new rectangle(25, 25, 1200, 600, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle184{new rectangle(125, 25, 1225, 600, sf::Color::White)};
+rectangle* Rectangle185{new rectangle(50, 25, 1350, 600, sf::Color::Black)};
+//Linea 21
+rectangle* Rectangle186{new rectangle(50, 25, 600, 625, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle187{new rectangle(25, 25, 675, 625, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle188{new rectangle(25, 25, 700, 625, sf::Color::Black)};
+rectangle* Rectangle189{new rectangle(25, 25, 725, 625, sf::Color::White)};
+rectangle* Rectangle190{new rectangle(25, 25, 750, 625, sf::Color::Black)};
+rectangle* Rectangle191{new rectangle(25, 25, 775, 625, sf::Color(245,151,0))};//Amarillo
+rectangle* Rectangle192{new rectangle(50, 25, 800, 625, sf::Color::White)};
+rectangle* Rectangle193{new rectangle(50, 25, 850, 625, sf::Color::Black)};
+rectangle* Rectangle194{new rectangle(25, 25, 900, 625, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle195{new rectangle(50, 25, 925, 625, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle196{new rectangle(50, 25, 975, 625, sf::Color::Black)};
+rectangle* Rectangle197{new rectangle(25, 25, 1025, 625, sf::Color::White)};
+rectangle* Rectangle198{new rectangle(125, 25, 1050, 625, sf::Color::Black)};
+rectangle* Rectangle199{new rectangle(25, 25, 1200, 625, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle200{new rectangle(175, 25, 1225, 625, sf::Color::White)};
+rectangle* Rectangle201{new rectangle(25, 25, 1400, 625, sf::Color::Black)};
+//Linea 22
+rectangle* Rectangle202{new rectangle(25, 25, 600, 650, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle203{new rectangle(25, 25, 625, 650, sf::Color::Black)};
+rectangle* Rectangle204{new rectangle(25, 25, 650, 650, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle205{new rectangle(25, 25, 700, 650, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle206{new rectangle(25, 25, 725, 650, sf::Color::Black)};
+rectangle* Rectangle207{new rectangle(25, 25, 750, 650, sf::Color(245,151,0))};//Amarillo
+rectangle* Rectangle208{new rectangle(25, 25, 775, 650, sf::Color::Black)};
+rectangle* Rectangle209{new rectangle(325, 25, 800, 650, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle210{new rectangle(50, 25, 1125, 650, sf::Color::Black)};
+rectangle* Rectangle211{new rectangle(25, 25, 1175, 650, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle212{new rectangle(25, 25, 1200, 650, sf::Color::Black)};
+rectangle* Rectangle213{new rectangle(125, 25, 1225, 650, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle214{new rectangle(50, 25, 1350, 650, sf::Color::White)};
+rectangle* Rectangle215{new rectangle(25, 25, 1400, 650, sf::Color::Black)};
+//Linea 23
+rectangle* Rectangle216{new rectangle(25, 25, 600, 675, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle217{new rectangle(25, 25, 650, 675, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle218{new rectangle(75, 25, 675, 675, sf::Color::Black)};
+rectangle* Rectangle219{new rectangle(25, 25, 750, 675, sf::Color(245,151,0))};//Amarillo
+rectangle* Rectangle220{new rectangle(375, 25, 775, 675, sf::Color::Black)};
+rectangle* Rectangle221{new rectangle(25, 25, 1150, 675, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle222{new rectangle(50, 25, 1175, 675, sf::Color::Black)};
+rectangle* Rectangle223{new rectangle(100, 25, 1225, 675, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle224{new rectangle(75, 25, 1325, 675, sf::Color::White)};
+rectangle* Rectangle225{new rectangle(25, 25, 1400, 675, sf::Color::Black)};
+//Linea 24
+rectangle* Rectangle226{new rectangle(50, 25, 600, 700, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle227{new rectangle(25, 25, 650, 700, sf::Color::Black)};
+rectangle* Rectangle228{new rectangle(25, 25, 750, 700, sf::Color::Black)};
+rectangle* Rectangle229{new rectangle(25, 25, 775, 700, sf::Color(245,151,0))};//Amarillo
+rectangle* Rectangle230{new rectangle(50, 25, 800, 700, sf::Color::White)};
+rectangle* Rectangle231{new rectangle(50, 25, 850, 700, sf::Color::Black)};
+rectangle* Rectangle232{new rectangle(50, 25, 900, 700, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle233{new rectangle(25, 25, 950, 700, sf::Color::Black)};
+rectangle* Rectangle234{new rectangle(75, 25, 975, 700, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle235{new rectangle(25, 25, 1050, 700, sf::Color::Black)};
+rectangle* Rectangle236{new rectangle(25, 25, 1075, 700, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle237{new rectangle(50, 25, 1100, 700, sf::Color::Black)};
+rectangle* Rectangle238{new rectangle(25, 25, 1150, 700, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle239{new rectangle(25, 25, 1175, 700, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle240{new rectangle(25, 25, 1200, 700, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle241{new rectangle(25, 25, 1225, 700, sf::Color::White)};
+rectangle* Rectangle242{new rectangle(50, 25, 1250, 700, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle243{new rectangle(100, 25, 1300, 700, sf::Color::White)};
+rectangle* Rectangle244{new rectangle(25, 25, 1400, 700, sf::Color::Black)};
+//Linea 25
+rectangle* Rectangle245{new rectangle(50, 25, 600, 725, sf::Color::Black)};
+rectangle* Rectangle246{new rectangle(50, 25, 775, 725, sf::Color::Black)};
+rectangle* Rectangle247{new rectangle(25, 25, 825, 725, sf::Color::White)};
+rectangle* Rectangle248{new rectangle(25, 25, 850, 725, sf::Color::Black)};
+rectangle* Rectangle249{new rectangle(50, 25, 875, 725, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle250{new rectangle(50, 25, 925, 725, sf::Color::Black)};
+rectangle* Rectangle251{new rectangle(50, 25, 975, 725, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle252{new rectangle(50, 25, 1025, 725, sf::Color::Black)};
+rectangle* Rectangle253{new rectangle(50, 25, 1075, 725, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle254{new rectangle(25, 25, 1125, 725, sf::Color::Black)};
+rectangle* Rectangle255{new rectangle(25, 25, 1200, 725, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle256{new rectangle(125, 25, 1225, 725, sf::Color::White)};
+rectangle* Rectangle257{new rectangle(50, 25, 1350, 725, sf::Color::Black)};
+//Linea 26
+rectangle* Rectangle258{new rectangle(125, 25, 825, 750, sf::Color::Black)};
+rectangle* Rectangle259{new rectangle(175, 25, 975, 750, sf::Color::Black)};
+rectangle* Rectangle260{new rectangle(150, 25, 1200, 750, sf::Color::Black)};
+//Linea 27
+rectangle* Rectangle261{new rectangle(50, 25, 825, 775, sf::Color::Black)};
+rectangle* Rectangle262{new rectangle(25, 25, 875, 775, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle263{new rectangle(25, 25, 900, 775, sf::Color::Black)};
+rectangle* Rectangle264{new rectangle(50, 25, 1050, 775, sf::Color::Black)};
+rectangle* Rectangle265{new rectangle(25, 25, 1100, 775, sf::Color(6,253,255))};//Azul celeste
+rectangle* Rectangle266{new rectangle(25, 25, 1125, 775, sf::Color::Black)};
+//Linea 28
+rectangle* Rectangle267{new rectangle(75, 25, 825, 800, sf::Color::Black)};
+rectangle* Rectangle268{new rectangle(75, 25, 1075, 800, sf::Color::Black)};
+//Linea 29
+rectangle* Rectangle269{new rectangle(75, 25, 800, 825, sf::Color::Black)};
+rectangle* Rectangle270{new rectangle(50, 25, 1100, 825, sf::Color::Black)};
+//Linea 30
+rectangle* Rectangle271{new rectangle(25, 25, 800, 850, sf::Color::Black)};
+rectangle* Rectangle272{new rectangle(50, 25, 825, 850, sf::Color(11,135,124))};//Azul Turquesa
+rectangle* Rectangle273{new rectangle(25, 25, 1100, 850, sf::Color::Black)};
+rectangle* Rectangle274{new rectangle(50, 25, 1125, 850, sf::Color(11,135,124))};//Azul Turquesa
 
 
 
@@ -355,6 +509,148 @@ void Game::Draw()
     window->draw(*Rectangle132->GetShape());
     window->draw(*Rectangle133->GetShape());
     window->draw(*Rectangle134->GetShape());
+    window->draw(*Rectangle135->GetShape());
+    window->draw(*Rectangle136->GetShape());
+    window->draw(*Rectangle137->GetShape());
+    window->draw(*Rectangle138->GetShape());
+    window->draw(*Rectangle139->GetShape());
+    window->draw(*Rectangle140->GetShape());
+    window->draw(*Rectangle141->GetShape());
+    window->draw(*Rectangle142->GetShape());
+    window->draw(*Rectangle143->GetShape());
+    window->draw(*Rectangle144->GetShape());
+    window->draw(*Rectangle145->GetShape());
+    window->draw(*Rectangle146->GetShape());
+    window->draw(*Rectangle147->GetShape());
+    window->draw(*Rectangle148->GetShape());
+    window->draw(*Rectangle149->GetShape());
+    window->draw(*Rectangle150->GetShape());
+    window->draw(*Rectangle151->GetShape());
+    window->draw(*Rectangle152->GetShape());
+    window->draw(*Rectangle153->GetShape());
+    window->draw(*Rectangle154->GetShape());
+    window->draw(*Rectangle155->GetShape());
+    window->draw(*Rectangle156->GetShape());
+    window->draw(*Rectangle157->GetShape());
+    window->draw(*Rectangle158->GetShape());
+    window->draw(*Rectangle159->GetShape());
+    window->draw(*Rectangle160->GetShape());
+    window->draw(*Rectangle161->GetShape());
+    window->draw(*Rectangle162->GetShape());
+    window->draw(*Rectangle163->GetShape());
+    window->draw(*Rectangle164->GetShape());
+    window->draw(*Rectangle165->GetShape());
+    window->draw(*Rectangle166->GetShape());
+    window->draw(*Rectangle167->GetShape());
+    window->draw(*Rectangle168->GetShape());
+    window->draw(*Rectangle169->GetShape());
+    window->draw(*Rectangle170->GetShape());
+    window->draw(*Rectangle171->GetShape());
+    window->draw(*Rectangle172->GetShape());
+    window->draw(*Rectangle173->GetShape());
+    window->draw(*Rectangle174->GetShape());
+    window->draw(*Rectangle175->GetShape());
+    window->draw(*Rectangle176->GetShape());
+    window->draw(*Rectangle177->GetShape());
+    window->draw(*Rectangle178->GetShape());
+    window->draw(*Rectangle179->GetShape());
+    window->draw(*Rectangle180->GetShape());
+    window->draw(*Rectangle181->GetShape());
+    window->draw(*Rectangle182->GetShape());
+    window->draw(*Rectangle183->GetShape());
+    window->draw(*Rectangle184->GetShape());
+    window->draw(*Rectangle185->GetShape());
+    window->draw(*Rectangle186->GetShape());
+    window->draw(*Rectangle187->GetShape());
+    window->draw(*Rectangle188->GetShape());
+    window->draw(*Rectangle189->GetShape());
+    window->draw(*Rectangle190->GetShape());
+    window->draw(*Rectangle191->GetShape());
+    window->draw(*Rectangle192->GetShape());
+    window->draw(*Rectangle193->GetShape());
+    window->draw(*Rectangle194->GetShape());
+    window->draw(*Rectangle195->GetShape());
+    window->draw(*Rectangle196->GetShape());
+    window->draw(*Rectangle197->GetShape());
+    window->draw(*Rectangle198->GetShape());
+    window->draw(*Rectangle199->GetShape());
+    window->draw(*Rectangle200->GetShape());
+    window->draw(*Rectangle201->GetShape());
+    window->draw(*Rectangle202->GetShape());
+    window->draw(*Rectangle203->GetShape());
+    window->draw(*Rectangle204->GetShape());
+    window->draw(*Rectangle205->GetShape());
+    window->draw(*Rectangle206->GetShape());
+    window->draw(*Rectangle207->GetShape());
+    window->draw(*Rectangle208->GetShape());
+    window->draw(*Rectangle209->GetShape());
+    window->draw(*Rectangle210->GetShape());
+    window->draw(*Rectangle211->GetShape());
+    window->draw(*Rectangle212->GetShape());
+    window->draw(*Rectangle213->GetShape());
+    window->draw(*Rectangle214->GetShape());
+    window->draw(*Rectangle215->GetShape());
+    window->draw(*Rectangle216->GetShape());
+    window->draw(*Rectangle217->GetShape());
+    window->draw(*Rectangle218->GetShape());
+    window->draw(*Rectangle219->GetShape());
+    window->draw(*Rectangle220->GetShape());
+    window->draw(*Rectangle221->GetShape());
+    window->draw(*Rectangle222->GetShape());
+    window->draw(*Rectangle223->GetShape());
+    window->draw(*Rectangle224->GetShape());
+    window->draw(*Rectangle225->GetShape());
+    window->draw(*Rectangle226->GetShape());
+    window->draw(*Rectangle227->GetShape());
+    window->draw(*Rectangle228->GetShape());
+    window->draw(*Rectangle229->GetShape());
+    window->draw(*Rectangle230->GetShape());
+    window->draw(*Rectangle231->GetShape());
+    window->draw(*Rectangle232->GetShape());
+    window->draw(*Rectangle233->GetShape());
+    window->draw(*Rectangle234->GetShape());
+    window->draw(*Rectangle235->GetShape());
+    window->draw(*Rectangle236->GetShape());
+    window->draw(*Rectangle237->GetShape());
+    window->draw(*Rectangle238->GetShape());
+    window->draw(*Rectangle239->GetShape());
+    window->draw(*Rectangle240->GetShape());
+    window->draw(*Rectangle241->GetShape());
+    window->draw(*Rectangle242->GetShape());
+    window->draw(*Rectangle243->GetShape());
+    window->draw(*Rectangle244->GetShape());
+    window->draw(*Rectangle245->GetShape());
+    window->draw(*Rectangle246->GetShape());
+    window->draw(*Rectangle247->GetShape());
+    window->draw(*Rectangle248->GetShape());
+    window->draw(*Rectangle249->GetShape());
+    window->draw(*Rectangle250->GetShape());
+    window->draw(*Rectangle251->GetShape());
+    window->draw(*Rectangle252->GetShape());
+    window->draw(*Rectangle253->GetShape());
+    window->draw(*Rectangle254->GetShape());
+    window->draw(*Rectangle255->GetShape());
+    window->draw(*Rectangle256->GetShape());
+    window->draw(*Rectangle257->GetShape());
+    window->draw(*Rectangle258->GetShape());
+    window->draw(*Rectangle259->GetShape());
+    window->draw(*Rectangle260->GetShape());
+    window->draw(*Rectangle261->GetShape());
+    window->draw(*Rectangle262->GetShape());
+    window->draw(*Rectangle263->GetShape());
+    window->draw(*Rectangle264->GetShape());
+    window->draw(*Rectangle265->GetShape());
+    window->draw(*Rectangle266->GetShape());
+    window->draw(*Rectangle267->GetShape());
+    window->draw(*Rectangle268->GetShape());
+    window->draw(*Rectangle269->GetShape());
+    window->draw(*Rectangle270->GetShape());
+    window->draw(*Rectangle271->GetShape());
+    window->draw(*Rectangle272->GetShape());
+    window->draw(*Rectangle273->GetShape());
+    window->draw(*Rectangle274->GetShape());
+
+
 
 
 
